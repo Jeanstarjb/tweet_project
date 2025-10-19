0146b93aa709eb3027fb2d95da26b0f3d3e680fe
Jeff Munyaka <161038752+Jeanstarjb@users.noreply.github.com>
2025-10-19 16:13:21 +0300

    Update model path for sentiment analysis

--- my_sentiment_app/app.py
+++ my_sentiment_app/app.py
@@ -124,7 +124,7 @@ st.set_page_config(
 @st.cache_resource
 def load_model():
     print("🚀 Loading the fine-tuned RoBERTa model...")
-    MODEL_PATH = "Shifterr/twitter-sentiment-roberta" # !! Make sure this matches your folder name !!
+    MODEL_PATH = "Shifterr/sentiment-model-roberta" # !! Make sure this matches your folder name !!
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
     model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
     print("✅ Model loaded successfully!")
